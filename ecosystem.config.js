1d39bdad73f41b941cb20b56164c503c1118af50
LukasKrisna <krisnaprasriyan@gmail.com>
2022-08-13 18:04:33 +0700

    make it better

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+module.exports = {
+    apps : [{
+      name: "notes-api",
+      script: "./src/server.js",
+      instances: "max",
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      }
+    }]
+  }
