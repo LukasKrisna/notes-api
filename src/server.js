c6edca60e0a5169105b2806705c7325975683625
LukasKrisna <krisnaprasriyan@gmail.com>
2022-08-13 18:41:04 +0700

    make it better

--- src/server.js
+++ src/server.js
@@ -4,8 +4,8 @@ const routes = require('./routes');
 
 const init = async () => {
     const server = Hapi.server({
-        port: 3000,
-        host: 'dicoding-notes.herokuapp.com/',
+        port: process.env.PORT,
+        host: process.env.NODE_ENV,
         routes: {
             cors: {
                 origin: ['*'],
